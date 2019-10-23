f884d553c4bf4bca011f98d2a0b6b9f99c2b7e1f
kakegawa <cmuwni@gmail.com>
2019-10-23 21:07:35 +0900

    404と500エラーページ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'ユーザー情報を変更しました' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
